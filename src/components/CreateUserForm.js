f9e8f4b7cab9a549f304a9a3064e1a208920b60f
github username <github email address>
2018-06-07 09:14:39 -0400

    created component shells. created folders for actions and reducers

---
+++ src/components/CreateUserForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CreateUserForm extends Component {
+
+  render() {
+    return (
+
+    )
+  }
+
+}
---
+++ src/components/StartBattleForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class StartBattleForm extends Component {
+
+  render() {
+    return (
+
+    )
+  }
+
+}
